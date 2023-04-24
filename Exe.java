b462b66303c6460d9036eb2d8a8d7cb7f180a96c
Alfredo <21030761@itcelaya.edu.mx>
2023-04-23 20:14:43 -0600

    Versión casi acabada del proyecto parcial 3

--- Exe.java
+++ Exe.java
@@ -8,21 +8,15 @@ public class Exe {
 
     public static void main(String[] args) {
 
-        /*Lista lista_espera = new Lista();
+        Lista_Procesos lista_espera = new Lista_Procesos();
         lista_espera.PUSH("Alfredo", 100, 10, 15);
-        lista_espera.PUSH("Chaires", 50, 20, 20);
-        lista_espera.PUSH("Oski", 1, 30, 15);
-        lista_espera.PUSH("Leal", 3, 30, 10);
-        lista_espera.PUSH("Mario", 10, 30, 45);
-        lista_espera.PUSH("Tristeza", 1, 30, 15);
-        lista_espera.PUSH("La miss", 3, 30, 10);
-        lista_espera.PUSH("Mi ruca", 10, 30, 45);
+        lista_espera.PUSH("Chaires", 50, 5, 12);
+        lista_espera.PUSH("Oski", 1, 15, 8);
+        lista_espera.PUSH("Leal", 3, 20, 3);
+        lista_espera.PUSH("Mario", 10, 15, 7);
+        lista_espera.PUSH("Tristeza", 1, 20, 4);
+        lista_espera.PUSH("La miss", 3, 10, 8);
         
-        Lista lista_ejecucion = new Lista();
-        
-        for (int i = 0; i < 3; i++) {
-            lista_ejecucion.PUSH(lista_espera.POP_start());
-        }
         
         String nombre;
         int numero, tamaño, tiempo;
@@ -30,9 +24,9 @@ public class Exe {
 
         String menuFormat = "Escoja una opción\n"
                 + "1.- Capturar nuevo proceso\n"
-                + "2.- Ordenar lista por tamaño\n"
+                /*+ "2.- Ordenar lista por tamaño\n"
                 + "3.- Ordenar lista por tiempo\n"
-                + "4- Ordenar lista por número\n"
+                + "4- Ordenar lista por número\n"*/
                 + "5.- Mostrar datos\n"
                 + "6.- Elimnar proceso por número\n"
                 + "7.- Salir del menú\n";
@@ -48,7 +42,7 @@ public class Exe {
                     tiempo = Integer.parseInt(JOptionPane.showInputDialog("Insertar tiempo: "));
                     lista_espera.PUSH(nombre, numero, tamaño, tiempo); 
                     break;
-                case 2:
+                /*case 2:
                     System.out.println("<----- Procesos ordenados por tamaño ----->");
                     lista_espera.ordenarXtamaño();
                     lista_espera.mostrarDatosX();
@@ -62,7 +56,7 @@ public class Exe {
                     System.out.println("<----- Procesos ordenados por número ----->");
                     lista_espera.ordenarXnumero();
                     lista_espera.mostrarDatosX();
-                    break;
+                    break;*/
                 case 5:
                     System.out.println("<----- Procesos registrados ----->\n");
                     lista_espera.mostrarDatosX();
@@ -75,7 +69,7 @@ public class Exe {
                     System.out.println("\nLa opción seleccionada no es válida");
                     break;
             }
-        }while(option != 7);*/
+        }while(option != 7);
         ordenarTiempos();
 
     }
